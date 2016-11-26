bd7124941bd16a9d16fbd702bcbb2575abe1fc49
riteshjha695 <riteshjha695@gmail.com>
2016-11-26 13:35:49 +0530

    Add files via upload

---
+++ Weather monitoring system/WeatherMonitoringSystem.ino
@@ -0,0 +1,98 @@
+#include <DHT.h>
+
+/* DHTServer - ESP8266 Webserver with a DHT sensor as an input
+
+   Based on ESP8266Webserver
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <DHT.h>
+#define DHTTYPE DHT22
+#define DHTPIN  13
+
+const char* ssid     = "XT1562 9667";
+const char* password = "password";
+
+ESP8266WebServer server(80);
+
+DHT dht(DHTPIN, DHTTYPE, 13); // 13 works fine for ESP8266
+ 
+float humidity, temp_f;  // Values read from sensor
+String webString="";     // String to display
+// Generally, you should use "unsigned long" for variables that hold time
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 2000;              // interval at which to read sensor
+ 
+void handle_root() {
+  server.send(500, "text/plain", "HELLO FROM THE WEATHER MONITORING SYSTEM, READ TEMPERATURE BY /temp AND HUMIDITY BY /humidity");
+  delay(100);
+}
+ 
+void setup(void)
+{
+  // You can open the Arduino IDE Serial Monitor window to see what the code is doing
+  Serial.begin(115200);  // Serial connection from ESP-01 via 3.3v console cable
+  dht.begin();           // initialize temperature sensor
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.print("\n\r \n\rWorking to connect");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("DHT Weather Reading Server");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+   
+  server.on("/", handle_root);
+  
+  server.on("/temp", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();       // read sensor
+    webString="CURRENT TEMPERATURE IS: "+String((int)temp_f)+" F";   // Arduino has a hard time with float to string
+    server.send(200, "text/plain", webString);            // send to someones browser when asked
+  });
+
+  server.on("/humidity", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+  gettemperature();           // read sensor
+    webString="CURRENT HUMIDITY IS: "+String((int)humidity)+"%";
+    server.send(200, "text/plain", webString);               // send to someones browser when asked
+  });
+  
+  server.begin();
+  Serial.println("HTTP server started");
+}
+ 
+void loop(void)
+{
+  server.handleClient();
+} 
+
+void gettemperature() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor 
+    previousMillis = currentMillis;   
+
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    temp_f = dht.readTemperature(true);     // Read temperature as Fahrenheit
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temp_f)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+  }
+}
---
+++ Wi-Fi controlled Robot/WIFIROBOT_F.ino
@@ -0,0 +1,151 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "XT1562 9667";
+const char* password = "password";
+ 
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT);
+  digitalWrite(D5, LOW);// at start turn off the GPIO 
+  digitalWrite(D6, LOW);// at start turn off the GPIO
+  digitalWrite(D7, LOW);// at start turn off the GPIO 
+  digitalWrite(D8, LOW);// at start turn off the GPIO
+   
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+   
+  WiFi.begin(ssid, password);
+   
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+   
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+    
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+   
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+   
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+   
+  // Match the request
+
+  
+    if (request.indexOf("/ROBOT=FORWARD") != -1) { //checks if you clicked FORWARD
+        digitalWrite(D5, HIGH);
+      digitalWrite(D6, LOW);
+      digitalWrite(D7, LOW);
+      digitalWrite(D8, HIGH);
+      //Serial.println("You clicked FORWARD");
+    }
+  
+
+  if (request.indexOf("/ROBOT=BACKWARD") != -1) { //checks if you clicked BACKWARD
+       digitalWrite(D5, LOW);
+      digitalWrite(D6, HIGH);
+      digitalWrite(D7, HIGH);
+      digitalWrite(D8, LOW);
+  
+     // Serial.println("You clicked BACKWARD");
+     
+  }
+
+     
+    if (request.indexOf("/ROBOT=LEFT") != -1) { //checks if you clicked LEFT
+       digitalWrite(D5, LOW);
+      digitalWrite(D6, LOW);
+      digitalWrite(D7, LOW);
+      digitalWrite(D8, HIGH);
+      //Serial.println("You clicked LEFT");
+      }
+
+
+      
+    if (request.indexOf("/ROBOT=RIGHT") != -1) { //checks if you clicked RIGHT
+        digitalWrite(D5, HIGH);
+      digitalWrite(D6, LOW);
+      digitalWrite(D7, LOW);
+      digitalWrite(D8, LOW);
+      //Serial.println("You clicked RIGHT");
+      }
+
+
+
+
+      
+    if (request.indexOf("/ROBOT=STOP") != -1) { //checks if you clicked STOP
+        digitalWrite(D5, LOW);
+      digitalWrite(D6, LOW);
+      digitalWrite(D7, LOW);
+      digitalWrite(D8, LOW);
+      //Serial.println("You clicked STOP");
+    
+  }
+ 
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+   
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+   
+  //client.print("ROBOT IS NOW: ");
+   
+  /*if(value == HIGH) {
+    client.print("On");  
+  } else {
+    client.print("Off");*/
+  
+  client.println("<br><br>");
+  client.println("Click <a href=\"/ROBOT=FORWARD\">here</a> to move the ROBOT FORWARD<br>");
+  client.println("Click <a href=\"/ROBOT=BACKWRD\">here</a> to move the ROBOT BACKWARD<br>");
+  client.println("Click <a href=\"/ROBOT=LEFT\">here</a> to move the ROBOT LEFT<br>");
+  client.println("Click <a href=\"/ROBOT=RIGHT\">here</a> to move the ROBOT RIGHT<br>");
+  client.println("Click <a href=\"/ROBOT=STOP\">here</a> to move the ROBOT STOP<br>");
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+    
+    
+    }
